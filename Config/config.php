574572438648a990b9786297e38d30c01a627b95
Duy-Kun <91863679+sherlockduy4869@users.noreply.github.com>
2022-07-02 14:45:42 +0700

    Feat: adding database for project

---
+++ Config/config.php
@@ -0,0 +1,6 @@
+<?php
+    define("DB_HOST", "fdb32.awardspace.net");
+    define("DB_USER", "4111175_webfinal");
+    define("DB_PASS", "J)C?%VON2O58GHUL"); //adwardspace
+    define("DB_NAME", "4111175_webfinal");
+?>
