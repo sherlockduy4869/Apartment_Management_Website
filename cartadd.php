2049ec3f28319333974f9ffa875da9745a3945e8
Duy-Kun <91863679+sherlockduy4869@users.noreply.github.com>
2022-07-02 19:32:35 +0700

    Move html file to folder html file, cut php for adding cart page

--- cartadd.php
+++ cartadd.php
@@ -19,8 +19,57 @@
 
     <h3 class="i-name">Add apartment cart</h3>
 
-    
+    <div class="boddyy">
+            <div class="container">
+                <div class="title">Add apartment cart</div>
 
+                <form action="#">
+                    <div class="user-details">
+                        <div class="input-box">
+                            <span class="details">Apartment code</span>
+                            <input type="text" placeholder="Enter apartment code" required>
+                        </div>
+                        <div class="input-box">
+                            <span class="details">Agent name</span>
+                            <input type="text" placeholder="Enter agent name" required>
+                        </div>
+                        <div class="input-box">
+                            <span class="details">Area</span>
+                            <select name="area" id="select_area">
+                                <option value="">--Type--</option>
+                                <option value="2">Best-Seller</option>
+                                <option value="1">Featured</option>
+                                <option value="0">Non-Featured</option>
+                            </select>
+                        </div>
+                        <div class="input-box">
+                            <span class="details">Bedroom</span>
+                            <input type="text" placeholder="Enter bedroom" required>
+                        </div>
+                        <div class="input-box">
+                            <span class="details">SQM</span>
+                            <input type="text" placeholder="Enter aparment area" required>
+                        </div>
+                        <div class="input-box">
+                            <span class="details">House owner</span>
+                            <input type="text" placeholder="Enter house owner name" required>
+                        </div>
+                        <div class="input-box">
+                            <span class="details">Phone</span>
+                            <input type="text" placeholder="Enter phone number" required>
+                        </div>
+                        <div class="input-box">
+                            <span class="details">House Owner Image</span>
+                            <input type="file" required>
+                        </div>        
+                    </div>
+                    <div class="button">
+                        <input class="btn btn-primary" type="submit" value="ADDING">
+                    </div>
+                </form>
+                
+            </div>
+        </div>
 </section>
 <?php 
     include_once "Include/footer.php";
