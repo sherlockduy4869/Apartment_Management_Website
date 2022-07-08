113e9bdf9db12efc00fe854aa610f8f0b136a347
Duy-Kun <91863679+sherlockduy4869@users.noreply.github.com>
2022-07-08 12:46:57 +0700

    Feat: adding 2 clases for apartNotRented and apartSelling, 2 class for adding as well

---
+++ apartNotRentedAdd.php
@@ -0,0 +1,99 @@
+<?php
+    include_once "Include/header.php";
+    include_once "Include/slider.php";
+    include_once $_SERVER['DOCUMENT_ROOT'].'/Class/apartNotRentedClass.php';
+?>
+<?php
+    // $apartCart = new apartcart();
+
+    // if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
+
+    //     $cartAdd = $apartCart->insert_apart_cart($_POST);
+    // }
+?>
+<section id="interface">
+    <div class="navigation">
+            <div class="n1">
+                <div>
+                    <i id="menu-btn" class="fas fa-bars"></i>
+                </div>
+                <span>APARTMENT CART</span>
+            </div>
+            <div class="profile">
+                <i class="fas fa-chart-bar"></i>
+                <img src="./Resource/img/profile-1.jpg">
+            </div>
+        </div>
+
+    <h3 class="i-name"></h3>
+
+    <div class="boddyy">
+            <div class="container">
+                <div class="title">Add Apartment Cart</div>
+
+                <form action="cartAdd.php" method="POST" enctype="multipart/form-data">
+                    <div class="user-details">
+                        <div class="input-box">
+                            <span class="details">Apartment code</span>
+                            <input type="text" name="apartment_code" placeholder="Enter apartment code" required>
+                        </div>
+                        <div class="input-box">
+                            <span class="details">Agency name</span>
+                            <input type="text" name="agent_name" placeholder="Enter agency name" required>
+                        </div>
+                        <div class="input-box">
+                            <span class="details">Area</span>
+                            <select name="area">
+                                <option value="Vinhomes Golden River">Vinhomes Golden River</option>
+                                <option value="Vinhomes Central Park">Vinhomes Central Park</option>
+                                <option value="Estella Height">Estella Height</option>
+                                <option value="Estella">Estella</option>
+                                <option value="Celesta">Celesta</option>
+                            </select>
+                        </div>
+                        <div class="input-box">
+                            <span class="details">Bedroom</span>
+                            <select name="bedroom">
+                                <option value="1 Bed">1 Bed</option>
+                                <option value="2 Bed">2 Bed</option>
+                                <option value="2 Bed + 1">2 Bed + 1</option>
+                                <option value="3 Bed">3 Bed</option>
+                                <option value="3 Bed + 1">3 Bed + 1</option>
+                                <option value="4 Bed">4 Bed</option>
+                                <option value="4 Bed + 1">4 Bed + 1</option>
+                            </select>
+                        </div>
+                        <div class="input-box">
+                            <span class="details">SQM</span>
+                            <input type="number" name="sqm" placeholder="Enter aparment area" required>
+                        </div>
+                        <div class="input-box">
+                            <span class="details">House owner</span>
+                            <input type="text" name="house_owner" placeholder="Enter house owner name" required>
+                        </div>
+                        <div class="input-box">
+                            <span class="details">Phone</span>
+                            <input type="text" name="phone_owner" placeholder="Enter phone number" required>
+                        </div>   
+                        <div class="input-box">
+                            <span class="details">Email</span>
+                            <input type="email" name="email_owner" placeholder="Enter email" required>
+                        </div>  
+                    </div>
+                    <?php 
+                    // if(isset($cartAdd))
+                    // {
+                    //     echo $cartAdd;
+                    // }
+                    // ?>
+                    <div class="button">
+                        <input class="btn btn-primary" name="submit" type="submit" value="ADDING">
+                    </div>
+                </form>
+
+            </div>
+        </div>
+</section>
+<?php 
+    include_once "Include/footer.php";
+?>
