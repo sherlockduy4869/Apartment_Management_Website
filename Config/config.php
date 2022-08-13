d7ea72c86cdb2eadeeb5800bf30998ed5b60c048
Duy-Kun <91863679+sherlockduy4869@users.noreply.github.com>
2022-08-14 03:44:43 +0700

    Change config database for website

--- Config/config.php
+++ Config/config.php
@@ -1,6 +1,6 @@
 <?php
-    define("DB_HOST", "fdb32.awardspace.net");
-    define("DB_USER", "4111175_webfinal");
-    define("DB_PASS", "J)C?%VON2O58GHUL"); //adwardspace
-    define("DB_NAME", "4111175_webfinal");
+    define("DB_HOST", "103.75.187.24");
+    define("DB_USER", "hsurjwbthosting_better_homes_company");
+    define("DB_PASS", "J)C?%VON2O58GHUL"); //INET
+    define("DB_NAME", "hsurjwbthosting_better_homes_company");
 ?>
--- apartTaxDetails.php
+++ apartTaxDetails.php
@@ -31,6 +31,7 @@
                 <span>APARTMENT RENTED TAX</span>
             </div>
             <div class="profile">
+                <i class="fas fa-chart-bar"></i>
                 <img src="./Resource/img/profile-1.jpg">
             </div>
         </div>
