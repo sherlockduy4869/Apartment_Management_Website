988a934ba7645676bd1afff4db7c1f1039947724
Duy-Kun <91863679+sherlockduy4869@users.noreply.github.com>
2022-08-15 05:41:05 +0700

    Recover to data analytics page

--- index.php
+++ index.php
@@ -2,145 +2,101 @@
     include_once "Include/header.php";
     include_once "Include/slider.php";
 ?>
-
 <section id="interface">
-    <div class="navigation">
-        <div class="n1">
-            <div>
-                <i id="menu-btn" class="fas fa-bars"></i>
+        <div class="navigation">
+            <div class="n1">
+                <div>
+                    <i id="menu-btn" class="fas fa-bars"></i>
+                </div>
+                <span>DATA ANALYTICS</span>
             </div>
-            <span>DATA ANALYTICS</span>
+            <div class="profile">
+                <img src="./Resource/img/profile-1.jpg">
+            </div>
         </div>
-        <div class="profile">
-            <img src="./Resource/img/profile-1.jpg">
-        </div>
-    </div>
-    <div class="insights">
-                
-                <!----------------------------END OF TOTAL---------------------------->
-                <div class="total">
-                    <span class="material-symbols-sharp">stacked_line_chart</span>
-                    <div class="middle">
-                        <div class="left">
-                            <h4>Total Income</h4>
-                            <h3>$25,024</h3>
-                        </div>
-                        <div class="percent">
-                            <svg>
-                                <circle cx="50" cy="50" r = "50"></circle>
-                                <circle cx="50" cy="50" r = "50" style="stroke-dashoffset: calc(314 - (314 * 50) / 100);"></circle>
-                            </svg>
-                            <div class="number">
-                                <h2>50%</h2>
-                            </div>
-                        </div>
-                    </div>
-                    <small class="text-muted">Last 24 Hours</small>
-                </div>
-                <!----------------------------END OF TOTAL---------------------------->
-                <div class="total">
-                    <span class="material-symbols-sharp">stacked_line_chart</span>
-                    <div class="middle">
-                        <div class="left">
-                            <h4>Total Income</h4>
-                            <h3>$25,024</h3>
-                        </div>
-                        <div class="percent">
-                            <svg>
-                                <circle cx="50" cy="50" r = "50"></circle>
-                                <circle cx="50" cy="50" r = "50" style="stroke-dashoffset: calc(314 - (314 * 50) / 100);"></circle>
-                            </svg>
-                            <div class="number">
-                                <h2>50%</h2>
-                            </div>
-                        </div>
-                    </div>
-                    <small class="text-muted">Last 24 Hours</small>
-                </div>
-                <!----------------------------END OF TOTAL---------------------------->
-                <div class="total">
-                    <span class="material-symbols-sharp">stacked_line_chart</span>
-                    <div class="middle">
-                        <div class="left">
-                            <h4>Total Income</h4>
-                            <h3>$25,024</h3>
-                        </div>
-                        <div class="percent">
-                            <svg>
-                                <circle cx="50" cy="50" r = "50"></circle>
-                                <circle cx="50" cy="50" r = "50" style="stroke-dashoffset: calc(314 - (314 * 50) / 100);"></circle>
-                            </svg>
-                            <div class="number">
-                                <h2>50%</h2>
-                            </div>
-                        </div>
-                    </div>
-                    <small class="text-muted">Last 24 Hours</small>
-                </div>
-                <!----------------------------END OF TOTAL---------------------------->
-                <div class="total">
-                    <span class="material-symbols-sharp">stacked_line_chart</span>
-                    <div class="middle">
-                        <div class="left">
-                            <h4>Total Income</h4>
-                            <h3>$25,024</h3>
-                        </div>
-                        <div class="percent">
-                            <svg>
-                                <circle cx="50" cy="50" r = "50"></circle>
-                                <circle cx="50" cy="50" r = "50" style="stroke-dashoffset: calc(314 - (314 * 50) / 100);"></circle>
-                            </svg>
-                            <div class="number">
-                                <h2>50%</h2>
-                            </div>
-                        </div>
-                    </div>
-                    <small class="text-muted">Last 24 Hours</small>
-                </div>
-                <!----------------------------END OF TOTAL---------------------------->
-                <div class="total">
-                    <span class="material-symbols-sharp">stacked_line_chart</span>
-                    <div class="middle">
-                        <div class="left">
-                            <h4>Total Income</h4>
-                            <h3>$25,024</h3>
-                        </div>
-                        <div class="percent">
-                            <svg>
-                                <circle cx="50" cy="50" r = "50"></circle>
-                                <circle cx="50" cy="50" r = "50" style="stroke-dashoffset: calc(314 - (314 * 50) / 100);"></circle>
-                            </svg>
-                            <div class="number">
-                                <h2>50%</h2>
-                            </div>
-                        </div>
-                    </div>
-                    <small class="text-muted">Last 24 Hours</small>
-                </div>
-                <!----------------------------END OF TOTAL---------------------------->
-                <div class="total">
-                    <span class="material-symbols-sharp">stacked_line_chart</span>
-                    <div class="middle">
-                        <div class="left">
-                            <h4>Total Income</h4>
-                            <h3>$25,024</h3>
-                        </div>
-                        <div class="percent">
-                            <svg>
-                                <circle cx="50" cy="50" r = "50"></circle>
-                                <circle cx="50" cy="50" r = "50" style="stroke-dashoffset: calc(314 - (314 * 50) / 100);"></circle>
-                            </svg>
-                            <div class="number">
-                                <h2>50%</h2>
-                            </div>
-                        </div>
-                    </div>
-                    <small class="text-muted">Last 24 Hours</small>
-                </div>
-                <!----------------------------END OF TOTAL---------------------------->
-    </div>
-</section>
 
+        <div class="insights">
+        <div class="sales">
+            <span class="material-symbols-sharp">analytics</span>
+            <div class="middle">
+                <div class="left">
+                    <h4>Total Sales</h4>
+                    <h3>$25,024</h3>
+                </div>
+                <div class="percent">
+                    <svg>
+                        <circle cx="50" cy="50" r = "50"></circle>
+                        <circle cx="50" cy="50" r = "50" style="stroke-dashoffset: calc(314 - (314 * 60) / 100);"></circle>
+                    </svg>
+                    <div class="number">
+                        <h2>60%</h2>
+                    </div>
+                </div>
+            </div>
+            <small class="text-muted">Last 24 Hours</small>
+        </div>
+        <!----------------------------END OF SALES---------------------------->
+        <div class="expenses">
+            <span class="material-symbols-sharp">bar_chart</span>
+            <div class="middle">
+                <div class="left">
+                    <h4>Total Expenses</h4>
+                    <h3>$25,024</h3>
+                </div>
+                <div class="percent">
+                    <svg>
+                        <circle cx="50" cy="50" r = "50"></circle>
+                        <circle cx="50" cy="50" r = "50" style="stroke-dashoffset: calc(314 - (314 * 30) / 100);"></circle>
+                    </svg>
+                    <div class="number">
+                        <h2>30%</h2>
+                    </div>
+                </div>
+            </div>
+            <small class="text-muted">Last 24 Hours</small>
+        </div>
+        <!----------------------------END OF EXPENSES---------------------------->
+        <div class="income">
+            <span class="material-symbols-sharp">stacked_line_chart</span>
+            <div class="middle">
+                <div class="left">
+                    <h4>Total Income</h4>
+                    <h3>$25,024</h3>
+                </div>
+                <div class="percent">
+                    <svg>
+                        <circle cx="50" cy="50" r = "50"></circle>
+                        <circle cx="50" cy="50" r = "50" style="stroke-dashoffset: calc(314 - (314 * 90) / 100);"></circle>
+                    </svg>
+                    <div class="number">
+                        <h2>90%</h2>
+                    </div>
+                </div>
+            </div>
+            <small class="text-muted">Last 24 Hours</small>
+        </div>
+        <!----------------------------END OF INCOME---------------------------->
+        <div class="total">
+            <span class="material-symbols-sharp">stacked_line_chart</span>
+            <div class="middle">
+                <div class="left">
+                    <h4>Total Income</h4>
+                    <h3>$25,024</h3>
+                </div>
+                <div class="percent">
+                    <svg>
+                        <circle cx="50" cy="50" r = "50"></circle>
+                        <circle cx="50" cy="50" r = "50" style="stroke-dashoffset: calc(314 - (314 * 50) / 100);"></circle>
+                    </svg>
+                    <div class="number">
+                        <h2>50%</h2>
+                    </div>
+                </div>
+            </div>
+            <small class="text-muted">Last 24 Hours</small>
+        </div>
+        <!----------------------------END OF TOTAL---------------------------->
+</section>
 <?php 
     include_once "Include/footer.php";
 ?>
