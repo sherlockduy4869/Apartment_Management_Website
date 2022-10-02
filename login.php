66b4a7ef6c65f4fd8540141ed4abc1922f5cf13f
Duy-Kun <91863679+sherlockduy4869@users.noreply.github.com>
2022-10-02 14:56:17 +0700

    remove QSDecor titile

--- login.php
+++ login.php
@@ -19,7 +19,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/Resource/css/styleLogin.css?v=<?php echo time(); ?>">
-    <title>QSDECOR</title>
+    <title>BETTER HOMES</title>
 </head>
 <body>
     <section>
