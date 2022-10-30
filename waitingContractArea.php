1b025337162edb5de23ba5b41a69eec6f6be9afb
Duy-Kun <91863679+sherlockduy4869@users.noreply.github.com>
2022-10-30 09:40:28 +0700

    Feat: adding waiting contract to slider

--- waitingContractArea.php
+++ waitingContractArea.php
@@ -8,7 +8,7 @@
                 <div>
                     <i id="menu-btn" class="fas fa-bars"></i>
                 </div>
-                <span>APARTMENT NEW CONTRACT</span>
+                <span>APARTMENT WAITING CONTRACT</span>
             </div>
             <div class="profile">
                 <img src="./Resource/img/profile-1.jpg">
@@ -19,11 +19,11 @@
         <div class="values">
             <div class="val-box clickable">
                 <i class="fas fa-info"></i>
-                <a href="newContract.php">New Contract No Tax</a>
+                <a href="waitingContractNoTax.php">Waiting Contract No Tax</a>
             </div>
             <div class="val-box clickable">
                 <i class="fas fa-info"></i>
-                <a href="newContractTax.php">New Contract Tax</a>
+                <a href="waitingContractTax.php">Waiting Contract Tax</a>
             </div>
         </div>
 </section>
